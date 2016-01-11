630ddc34932f708a457979a52baf82e0a837f76d
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-01-11 17:29:48 +0000

    fizzbuzz done with tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,17 @@
 require 'fizzbuzz'
 
-describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect (fizzbuzz(3)).to eq "fizz"
+describe 'fizz' do
+  it 'returns "fizz" when passed multiples of 3, that isnt also a multiple of 5' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+describe 'buzz'
+  it 'returns "buzz" when passed multiples of 5 that isnt also a multiple of 3' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+describe 'fizzbuzz'
+  it "returns 'fizzbuzz' when passed multiples of 5 and 3" do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
