9934c5e70568a4709e6110a394337099433bf4df
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-01-11 22:12:14 +0000

    second time

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,3 @@
-
-def fizzbuzz(number)
-  if number % 5 != 0 && number % 3 != 0
-    return "NO BUZZ, NO FIZZ BABY"
-  elsif number % 5 == 0 && number % 3 == 0
-    return 'fizzbuzz'
-  else
-    number % 5 == 0 ? "buzz" : "fizz"
-  end
+def fizzbuzz (number)
+number
 end
