666521c913b7fd2c0259fd5fa31bd470e699b26a
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-01-11 17:55:21 +0000

    refactored

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,10 @@
+
 def fizzbuzz(number)
-  if number % 5 == 0 && number % 3 == 0
-    return 'fizzbuzz'
-  elsif number % 3 == 0
-    return 'fizz'
-  elsif number % 5 == 0
-    return 'buzz'
-  else
+  if number % 5 != 0 && number % 3 != 0
     return "NO BUZZ, NO FIZZ BABY"
+  elsif number % 5 == 0 && number % 3 == 0
+    return 'fizzbuzz'
+  else
+    number % 5 == 0 ? "buzz" : "fizz"
   end
 end
